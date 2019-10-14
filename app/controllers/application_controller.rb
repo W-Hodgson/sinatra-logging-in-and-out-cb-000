4e9d863668faed7c151bfefae9ada0a3d253ec5b
Will Hodgson <will.hodgson@current-rms.com>
2019-10-14 10:59:21 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,34 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-
+    @user = User.find_by(username: params[:username], password: params[:password])
+    if @user
+      session[:user_id] = @user.id
+      redirect to '/account'
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/account' do
-
+    if Helpers.is_logged_in?(session)
+      @user = User.find_by(id: session[:user_id])
+      erb :account
+    else
+      erb :error
+    end
   end
 
   get '/logout' do
+    session.clear
+    redirect '/'
+  end
 
+  post '/logout' do
+    session.clear
+    redirect '/'
   end
 
 
 end
-
